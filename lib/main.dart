05a0d661ff0b1981051db6d18a1b7baec2e6421a
yayan_agustyan <yayanagustyan@gmail.com>
2024-03-19 20:49:29 +0700

    logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_share/flutter_share.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
@@ -24,15 +22,13 @@ class MyApp extends StatelessWidget {
         colorScheme: const ColorScheme.dark(),
         useMaterial3: false,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -42,34 +38,55 @@ class _MyHomePageState extends State<MyHomePage> {
   var scController = ScreenshotController();
 
   var clubs = [
-    {"code": "---", "name": "- - -"},
-    {"code": "ALV", "name": "Alaves"},
-    {"code": "UDA", "name": "Almeria"},
-    {"code": "ATB", "name": "Athletic Bilbao"},
-    {"code": "ATM", "name": "Atletico de Madrid"},
-    {"code": "FCB", "name": "FC Barcelona"},
-    {"code": "BET", "name": "Real Betis"},
-    {"code": "CLV", "name": "Celta de Vigo"},
-    {"code": "CCF", "name": "Cadiz"},
-    {"code": "GET", "name": "Getafe"},
-    {"code": "GIR", "name": "Girona"},
-    {"code": "GCF", "name": "Granada"},
-    {"code": "LAP", "name": "Las Palmas"},
-    {"code": "RCD", "name": "Mallorca"},
-    {"code": "OSA", "name": "Osasuna"},
-    {"code": "SOC", "name": "Real Sociedad"},
-    {"code": "RAY", "name": "Rayo Vallecano"},
-    {"code": "MAD", "name": "Real Madrid"},
-    {"code": "SEV", "name": "Sevilla"},
-    {"code": "VAL", "name": "Valencia"},
-    {"code": "VIL", "name": "Villarreal"}
+    {"code": "---", "logo": "default.jpg", "name": "- - -"},
+    {"code": "ALV", "logo": "ALV.png", "name": "Alaves"},
+    {"code": "UDA", "logo": "UDA.png", "name": "Almeria"},
+    {"code": "ATB", "logo": "ATB.png", "name": "Athletic Bilbao"},
+    {"code": "ATM", "logo": "ATM.png", "name": "Atletico de Madrid"},
+    {"code": "FCB", "logo": "FCB.png", "name": "FC Barcelona"},
+    {"code": "BET", "logo": "BET.png", "name": "Real Betis"},
+    {"code": "CLV", "logo": "CLV.png", "name": "Celta de Vigo"},
+    {"code": "CCF", "logo": "CCF.png", "name": "Cadiz"},
+    {"code": "GET", "logo": "GET.png", "name": "Getafe"},
+    {"code": "GIR", "logo": "GIR.png", "name": "Girona"},
+    {"code": "GCF", "logo": "GCF.png", "name": "Granada"},
+    {"code": "LAP", "logo": "LAP.png", "name": "Las Palmas"},
+    {"code": "RCD", "logo": "RCD.png", "name": "Mallorca"},
+    {"code": "OSA", "logo": "OSA.png", "name": "Osasuna"},
+    {"code": "SOC", "logo": "SOC.png", "name": "Real Sociedad"},
+    {"code": "RAY", "logo": "RAY.png", "name": "Rayo Vallecano"},
+    {"code": "MAD", "logo": "MAD.png", "name": "Real Madrid"},
+    {"code": "SEV", "logo": "SEV.png", "name": "Sevilla"},
+    {"code": "VAL", "logo": "VAL.png", "name": "Valencia"},
+    {"code": "VIL", "logo": "VIL.png", "name": "Villarreal"}
   ];
 
   List<Widget> items() {
     List<Widget> ele = [];
     for (var item in clubs) {
       ele.add(Center(
-        child: Text(item.entries.last.value),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                border: Border.all(color: Colors.white),
+              ),
+              height: 30,
+              width: 30,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  "assets/images/${item['logo']}",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(width: 15),
+            Text(item.entries.last.value),
+          ],
+        ),
       ));
     }
     return ele;
@@ -103,6 +120,9 @@ class _MyHomePageState extends State<MyHomePage> {
   bool anime2n = false;
   bool animate3 = false;
   bool anime3n = false;
+
+  String imgHome = "default.jpg";
+  String imgAway = "default.jpg";
 
   Timer? checker;
   setTimer() {
@@ -185,23 +205,74 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Column(
           children: [
+            const SizedBox(height: 100),
             Container(
-              height: 250,
+              padding: const EdgeInsets.all(10),
               color: Colors.black,
-            ),
-            Container(
-              width: double.infinity,
-              color: Colors.black,
-              padding: const EdgeInsets.all(8.0),
-              child: const Text(
-                "SPANISH LALIGA",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.white),
+                    ),
+                    height: 80,
+                    width: 80,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/images/$imgHome",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Goal Guess",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text("Football Score Predictor"),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: Colors.white),
+                    ),
+                    height: 80,
+                    width: 80,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        "assets/images/$imgAway",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
+            // Container(
+            //   width: double.infinity,
+            //   color: Colors.black,
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: const Text(
+            //     "SPANISH LALIGA",
+            //     textAlign: TextAlign.center,
+            //     style: TextStyle(fontWeight: FontWeight.bold),
+            //   ),
+            // ),
             Column(
               children: [
                 Container(
@@ -247,13 +318,14 @@ class _MyHomePageState extends State<MyHomePage> {
                                   backgroundColor: Colors.red,
                                 )
                               : const BoxDecoration(color: Colors.red),
-                          height: 50,
+                          height: 100,
                           child: Center(
                               child: Text(
                             home,
+                            textAlign: TextAlign.center,
                             style: const TextStyle(
                               fontWeight: FontWeight.bold,
-                              fontSize: 24,
+                              fontSize: 22,
                             ),
                           )),
                         ),
@@ -262,7 +334,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Expanded(
                       child: Container(
                         color: Colors.black,
-                        height: 50,
+                        height: 100,
                         child: Center(
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -352,20 +424,21 @@ class _MyHomePageState extends State<MyHomePage> {
                                   gap: 5,
                                 )
                               : const BoxDecoration(
-                                  gradient: LinearGradient(
-                                    stops: [0, 1],
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
-                                    colors: [Colors.white, Colors.black],
+                                  // gradient: LinearGradient(
+                                  //   stops: [0, 1],
+                                  //   begin: Alignment.topLeft,
+                                  //   end: Alignment.bottomRight,
+                                  //   colors: [Colors.white, Colors.black],
+                                  // ),
                                   ),
-                                ),
-                          height: 50,
+                          height: 100,
                           child: Center(
                             child: Text(
                               away,
+                              textAlign: TextAlign.center,
                               style: const TextStyle(
                                 fontWeight: FontWeight.bold,
-                                fontSize: 24,
+                                fontSize: 22,
                               ),
                             ),
                           ),
@@ -402,7 +475,58 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(height: 50, color: Colors.black),
+                      Container(
+                        color: Colors.black,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                border: Border.all(color: Colors.white),
+                              ),
+                              height: 80,
+                              width: 80,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.asset(
+                                  "assets/images/$imgHome",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Goal Guess",
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text("Football Score Predictor"),
+                              ],
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                border: Border.all(color: Colors.white),
+                              ),
+                              height: 80,
+                              width: 80,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.asset(
+                                  "assets/images/$imgAway",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                       Container(
                         width: MediaQuery.of(context).size.width,
                         color: Colors.black,
@@ -433,10 +557,11 @@ class _MyHomePageState extends State<MyHomePage> {
                             child: Container(
                               decoration:
                                   const BoxDecoration(color: Colors.red),
-                              height: 50,
+                              height: 100,
                               child: Center(
                                   child: Text(
                                 home,
+                                textAlign: TextAlign.center,
                                 style: const TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 24,
@@ -502,10 +627,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                   colors: [Colors.white, Colors.black],
                                 ),
                               ),
-                              height: 50,
+                              height: 100,
                               child: Center(
                                 child: Text(
                                   away,
+                                  textAlign: TextAlign.center,
                                   style: const TextStyle(
                                     fontWeight: FontWeight.bold,
                                     fontSize: 24,
@@ -568,12 +694,14 @@ class _MyHomePageState extends State<MyHomePage> {
                       onSelectedItemChanged: (int value) {
                         setState(() {
                           if (idx == "0") {
-                            home = clubs[value].entries.first.value;
+                            home = clubs[value].entries.last.value;
                             homeL = clubs[value].entries.last.value;
+                            imgHome = clubs[value]['logo']!;
                           }
                           if (idx == "3") {
-                            away = clubs[value].entries.first.value;
+                            away = clubs[value].entries.last.value;
                             awayL = clubs[value].entries.last.value;
+                            imgAway = clubs[value]['logo']!;
                           }
                         });
                       },
